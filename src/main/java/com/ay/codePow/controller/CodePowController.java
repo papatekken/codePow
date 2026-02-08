c45ea8d0c66cbc677e22350aa1a5fa08f631d7df
Allan Yip <papatekken@gmail.com>
2026-02-08 11:11:35 +0000

    WIP update backbone

--- src/main/java/com/ay/codePow/controller/CodePowController.java
+++ src/main/java/com/ay/codePow/controller/CodePowController.java
@@ -1,7 +1,7 @@
 package com.ay.codePow.controller;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+
 public class CodePowController {
 
 }
--- src/test/java/com/ay/codePow/CodePowApplicationTests.java
+++ src/test/java/com/ay/codePow/CodePowApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CodePowApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
